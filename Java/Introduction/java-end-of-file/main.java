d3ba9bdef10b5cecf6e5d12301c43ed9b75ea021
Mario Valentin Ochoa Mota <mvochoaa@gmail.com>
2018-12-29 23:10:41 -0600

    Solución al ejercicio: Java End-of-file

---
+++ Java/Introduction/java-end-of-file/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; sc.hasNext(); i++) {
+            System.out.println(i + " " + sc.nextLine());
+        }
+    }
+}
