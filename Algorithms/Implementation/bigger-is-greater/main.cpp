4d3cda29a467fb530cb54ecf7052c99ae3ea7702
Mario Valentin Ochoa Mota <mvochoaa@gmail.com>
2019-01-26 13:14:13 -0600

    Otra solución mas efectiva al ejercicio: Bigger is Greater

---
+++ Algorithms/Implementation/bigger-is-greater/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int test;
+    cin >> test;
+    while(test--) {
+        string inp;
+        cin >> inp;
+        if (next_permutation(inp.begin(), inp.end()) == false ) {
+            cout << "no answer" << endl;
+        }
+        else {
+            cout << inp << endl;
+        }
+    }
+    return 0;
+}
